d3cdee794fb6b113e2e0a006e340a87b0393a65b
vinaykr <vinay@revinate.com>
2015-11-03 16:58:33 -0800

    including dimension info with documents too
    Change-Id: I01115874e48f60267925754a1ba2fa828dd97a9b

--- Result/Documents.php
+++ Result/Documents.php
@@ -2,6 +2,8 @@
 
 namespace Revinate\AnalyticsBundle\Result;
 
+use Revinate\AnalyticsBundle\FilterSource\FilterSourceInterface;
+
 class Documents extends AbstractResult {
 
     /**
@@ -9,9 +11,23 @@ class Documents extends AbstractResult {
      */
     public function getResult() {
         $documents = array();
+        /** @var FilterSourceInterface[] $fieldToFilterSource */
+        $fieldToFilterSource = array();
+        foreach ($this->analytics->getDimensions() as $dimension) {
+            if ($filterSource = $dimension->getFilterSource()) {
+                $fieldToFilterSource[$dimension->getField()] = $filterSource;
+            }
+        }
+
         foreach ($this->getElasticaResultSet()->getResults() as $result) {
             $doc = $result->getData();
             $doc["_id"] = $result->getId();
+            foreach ($fieldToFilterSource as $field => $filterSource) {
+                if (isset($doc[$field])) {
+                    // TODO: replace get() with mget()
+                    $doc[$field . "_info"] = $filterSource->get($doc[$field]);
+                }
+            }
             $documents[] = $doc;
         }
         return $documents;
