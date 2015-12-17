5eaa0c1c81cdf674f7a83f55728f07a0674f85f2
Michael Tango <michael@revinate.com>
2015-12-17 16:35:04 -0500

    REV-12811 Add generic last x month and last x month to date periods w/ tests
    Change-Id: I3be577776bb185e43aca6a218a34c643b6210500

--- Test/TestCase/Helper/DateHelperTest.php
+++ Test/TestCase/Helper/DateHelperTest.php
@@ -21,5 +21,33 @@ class DateHelperTest extends \PHPUnit_Framework_TestCase {
 
         $this->assertEquals($lmpTypicalExpectedResults, DateHelper::getPeriodInfo('lmp', strtotime('November 16 2015')));
         $this->assertEquals($lmpEdgeExpectedResults, DateHelper::getPeriodInfo('lmp', strtotime('March 31 2015')));
+
+        $l3mExpectedResults = array(
+            'period' => array('2015-09-01', 'last_3_month', '2015-11-30'),
+            'description' => 'Last 3 Months: Sep - Nov',
+            'short_description' => 'Last 3 Months'
+        );
+        $l6mExpectedResults = array(
+            'period' => array('2015-06-01', 'last_6_month', '2015-11-30'),
+            'description' => 'Last 6 Months: Jun - Nov',
+            'short_description' => 'Last 6 Months'
+        );
+
+        $this->assertEquals($l3mExpectedResults, DateHelper::getPeriodInfo('l3m', strtotime('December 15 2015')));
+        $this->assertEquals($l6mExpectedResults, DateHelper::getPeriodInfo('l6m', strtotime('December 15 2015')));
+
+        $l3mtdExpectedResults = array(
+            'period' => array('2015-09-01', 'last_3_month_to_date', '2015-12-15'),
+            'description' => 'Last 3 Months to date: Sep - 2015-12-15',
+            'short_description' => 'Last 3 Months to Date'
+        );
+        $l6mtdExpectedResults = array(
+            'period' => array('2015-06-01', 'last_6_month_to_date', '2015-12-15'),
+            'description' => 'Last 6 Months to date: Jun - 2015-12-15',
+            'short_description' => 'Last 6 Months to Date'
+        );
+
+        $this->assertEquals($l3mtdExpectedResults, DateHelper::getPeriodInfo('l3mtd', strtotime('December 15 2015')));
+        $this->assertEquals($l6mtdExpectedResults, DateHelper::getPeriodInfo('l6mtd', strtotime('December 15 2015')));
     }
 }
